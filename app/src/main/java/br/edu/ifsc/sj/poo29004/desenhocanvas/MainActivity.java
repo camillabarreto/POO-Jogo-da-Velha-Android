ce494d9a918810eb5b36ec41d50b139330c62914
Emerson Ribeiro de Mello <emersonmello@users.noreply.github.com>
2018-11-27 06:27:36 -0200

    Adicionando diálogo de confirmação no evento onBackPressed da MainActivity

--- app/src/main/java/br/edu/ifsc/sj/poo29004/desenhocanvas/MainActivity.java
+++ app/src/main/java/br/edu/ifsc/sj/poo29004/desenhocanvas/MainActivity.java
@@ -1,5 +1,7 @@
 package br.edu.ifsc.sj.poo29004.desenhocanvas;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(R.string.msgSaida)
+                .setCancelable(false)
+                .setPositiveButton(R.string.sim, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        finishAndRemoveTask();
+                    }
+                })
+                .setNegativeButton(R.string.nao, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
     }
 }
